3d78299a357b77c5ddaf388e924a0ad224c4ea89
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-05-19 10:54:07 -0700

    add watch to index controller

--- app/Index.controller.js
+++ app/Index.controller.js
@@ -5,13 +5,13 @@
     .module('gDating')
     .controller('Index', Index);
 
-  Index.$inject = ['User', '$watch'];
+  Index.$inject = ['User', '$scope'];
 
-  function Index(User, $watch) {
+  function Index(User, $scope) {
     var vm = this;
 
     vm.message = 'Message from the Index controller';
-    vm.$watch(User.currentUser, function(user) {
+    $scope.$watch(User.currentUser, function(user) {
       vm.currentUser = User.currentUser();
     });
     vm.logout = function() {

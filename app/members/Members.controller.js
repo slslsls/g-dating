b9fd77df985e9cb86856c15832a1ca2c12642ecb
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-05-21 22:14:26 -0700

    add gender and interestedIn logic to members controller

--- app/members/Members.controller.js
+++ app/members/Members.controller.js
@@ -9,13 +9,19 @@
     var vm = this;
 
     vm.members = [];
+    vm.genders = Members.genders;
+    vm.interestedIns = Members.interestedIns;
     vm.thisYear = (new Date).getFullYear();
     Members.getAllMembers().then(function(data) {
-      for (var i = 5; i < 25; i++) {
+      for (var i = 5; i < 10; i++) {
         var member = data.data.data[i];
         var birthday = member.dob.substring(0,4);
+        var gender = vm.genders[member.gender];
+        var interestedIn = vm.interestedIns[member.interestedIn];
 
         member.age = vm.thisYear - birthday;
+        member.gender = gender;
+        member.interestedIn = interestedIn;
         vm.members.push(member);
       }
     })
--- app/members/Members.factory.js
+++ app/members/Members.factory.js
@@ -9,8 +9,20 @@
 
   function Members($http) {
     var api = 'http://galvanize-student-apis.herokuapp.com/gdating';
-    
+
     return {
+      genders: [
+        'male',
+        'female',
+        'transgender',
+        'non-binary'
+      ],
+      interestedIns: [
+        'men',
+        'women',
+        'transgender',
+        'non-binary'
+      ],
       getAllMembers: function() {
         return $http.get(api + '/members');
       },
